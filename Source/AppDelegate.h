f92bf538383fafb01a0fef51ce6f65082cfa9a21
Andrey Shevtsov <shevtsov.ap@gmail.com>
2016-01-20 11:07:49 -0800

    initial commit

---
+++ Source/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIProject
+//
+//  Created by Andrey Shevtsov on 1/20/16.
+//  Copyright (c) 2016 Andrey Shevtsov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Source/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIProject
+//
+//  Created by Andrey Shevtsov on 1/20/16.
+//  Copyright (c) 2016 Andrey Shevtsov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
